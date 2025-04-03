0c649c6d9cf64a0bb9d4a89e1947ac23bf72a5ff
Nissi Daniella Diallo <nissidaniella@yahoo.com>
2025-04-03 02:45:42 +0000

    last commit

--- pizza-web/src/pages/Contact.jsx
+++ pizza-web/src/pages/Contact.jsx
@@ -5,8 +5,23 @@ import React from 'react'
 const Contact = () => {
   return (
     <div>
+       <div className="flex justify-center items-center h-[150px] flex-row gap-[160px]">
+       <h1 className="  text-[60px] font-bold text-red-200"> NissiPizza </h1><h2 className='text-[14px]'>Lorem ipsum dolor.</h2>
+
+       <div className="contact-info flex flex-col text-[14px]">
+        <h2>Phone +234 567 890</h2>
+        <h2>Email <span>nissi@gmail.com</span></h2>
+        <h2>Address <span>123 Main Street, </span></h2>
+       </div>
+
+       <div className="socials-info flex flex-col text-[14px]">
+        <h2>Instagram</h2>
+        <h2>Facebook</h2>
+        <h2>TikTok</h2>
+       </div>
+
        
-       contact
+       </div>
     </div>
   )
 }
